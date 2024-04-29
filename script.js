b542c262156b9e1390ff6a5573ddbef6e7b64f57
Shordd <84244469+Shordd@users.noreply.github.com>
2024-04-29 14:49:33 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Add event listener to navigation links
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', event => {
-    event.preventDefault();
-    const target = event.target.getAttribute('href').replace(/^#/, ''); // Remove the # character
-    document.querySelector(`#${target}`).scrollIntoView({ behavior: 'smooth' }); // Add the # character back
-  });
-});
